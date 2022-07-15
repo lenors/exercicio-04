47fa7b2380f3b510f38d5f3bbab5b2193e9d038b
Leno rafael <73203800+lenors@users.noreply.github.com>
2022-07-15 14:14:53 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+export default class desafio extends Component{
+  arrow = () => {
+    return(
+          <div>
+            <h1>Olá, mundo</h1>
+          </div>
+    )
+    
+  }
+  soma = () =>{ 
+    return (
+      <div>
+        <h2>{4*2}</h2>
+      </div>
+    )
+  }
+  render(){
+    return(
+      <div>
+        {this.arrow()}
+        {this.soma()}
+      </div>
+    )
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
 }
-
-export default App;
